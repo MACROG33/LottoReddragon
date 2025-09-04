5a0e47c1dcaab879d85ba811ec3f205839111457
PalmKitsanaphon <Kitsanaphon.sukdee@gmail.com>
2025-09-04 17:09:55 +0700

    Update page_register.dart

--- lotto/lib/pages/page_register.dart
+++ lotto/lib/pages/page_register.dart
@@ -8,28 +8,31 @@ class PageRegister extends StatefulWidget {
 }
 
 class _PageRegisterState extends State<PageRegister> {
+  final TextEditingController _dateOfBirthController = TextEditingController();
+  DateTime? _selectedDate;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFD10922),
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.all(16),
+          padding: EdgeInsets.all(36),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: 40),
+              SizedBox(height: 50),
               Text(
                 'สมัครสมาชิกผู้ใช้งาน',
                 style: TextStyle(
-                  fontSize: 24,
+                  fontSize: 30,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-
+              SizedBox(height: 25),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
+
                 children: [
                   Text('ชื่อจริง', style: TextStyle(color: Colors.white)),
                   TextField(
@@ -39,6 +42,7 @@ class _PageRegisterState extends State<PageRegister> {
                       border: OutlineInputBorder(),
                     ),
                   ),
+                  SizedBox(height: 10),
                   Text('นามสกุล', style: TextStyle(color: Colors.white)),
                   TextField(
                     decoration: InputDecoration(
@@ -47,14 +51,20 @@ class _PageRegisterState extends State<PageRegister> {
                       border: OutlineInputBorder(),
                     ),
                   ),
+                  SizedBox(height: 10),
                   Text('วันเกิด', style: TextStyle(color: Colors.white)),
                   TextField(
+                    controller: _dateOfBirthController,
+                    readOnly: true,
+                    onTap: () => _selectDate(context),
                     decoration: InputDecoration(
                       filled: true,
                       fillColor: Colors.white,
                       border: OutlineInputBorder(),
+                      suffixIcon: Icon(Icons.calendar_today),
                     ),
                   ),
+                  SizedBox(height: 10),
                   Text('อีเมล์', style: TextStyle(color: Colors.white)),
                   TextField(
                     decoration: InputDecoration(
@@ -63,6 +73,7 @@ class _PageRegisterState extends State<PageRegister> {
                       border: OutlineInputBorder(),
                     ),
                   ),
+                  SizedBox(height: 10),
                   Text('รหัสผ่าน', style: TextStyle(color: Colors.white)),
                   TextField(
                     decoration: InputDecoration(
@@ -71,6 +82,7 @@ class _PageRegisterState extends State<PageRegister> {
                       border: OutlineInputBorder(),
                     ),
                   ),
+                  SizedBox(height: 10),
                   Text('ยืนยันรหัสผ่าน', style: TextStyle(color: Colors.white)),
                   TextField(
                     decoration: InputDecoration(
@@ -79,6 +91,7 @@ class _PageRegisterState extends State<PageRegister> {
                       border: OutlineInputBorder(),
                     ),
                   ),
+                  SizedBox(height: 10),
                   Text(
                     'จำนวนเงินในระบบ',
                     style: TextStyle(color: Colors.white),
@@ -90,19 +103,29 @@ class _PageRegisterState extends State<PageRegister> {
                       border: OutlineInputBorder(),
                     ),
                   ),
+                  SizedBox(height: 10),
                   Center(
                     child: FilledButton(
+                      style: FilledButton.styleFrom(
+                        backgroundColor: Color(0xFFFFD700),
+                        textStyle: TextStyle(fontWeight: FontWeight.bold),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        foregroundColor: Colors.black,
+                      ),
                       onPressed: () {},
                       child: Text('สมัครสมาชิก'),
                     ),
                   ),
+                  SizedBox(height: 10),
                   Row(
                     mainAxisAlignment:
                         MainAxisAlignment.center, // จัดตรงกลางแถว
                     children: [
                       Text(
                         'มีบัญชีอยู่แล้ว?',
-                        style: TextStyle(color: Colors.black),
+                        style: TextStyle(color: Colors.white),
                       ),
                       Text(
                         ' เข้าสู่ระบบ',
@@ -117,5 +140,33 @@ class _PageRegisterState extends State<PageRegister> {
         ),
       ),
     );
+  }
+  //Progress Date Picker
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate ?? DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData.light().copyWith(
+            colorScheme: ColorScheme.light(
+              primary: Color(0xFFD10922),
+              onPrimary: Colors.white,
+              onSurface: Colors.black,
+            ),
+          ),
+          child: child!,
+        );
+      },
+    );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+        _dateOfBirthController.text =
+            "${picked.day}/${picked.month}/${picked.year}";
+      });
+    }
   }
 }
