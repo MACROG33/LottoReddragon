5f30d5b519a1708c4738fddd4567285ec79ce7a2
PalmKitsanaphon <Kitsanaphon.sukdee@gmail.com>
2025-09-05 17:52:59 +0700

    Update

--- lotto/lib/main.dart
+++ lotto/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lotto/pages/page_search_lotto.dart';
+import 'package:lotto/pages/page_history_lotto.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const PageSearchLotto(),
+      home: const PageHistoryLotto(),
     );
   }
 }
